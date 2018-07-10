7ae6dd1eb4e23a66cb594d69c581e163c20e474c
Darin Burleigh <darin.burleigh@cdw.com>
2018-07-10 12:41:58 -0500

    logging

--- gproject/projects.py
+++ gproject/projects.py
@@ -17,16 +17,19 @@ plist.list()
 
 """
 
-
 import os.path
 import re
 import csv
 import subprocess
-#import yaml
+
+#from ruamel.yaml import YAML
 from ruamel.yaml import YAML
+import logging
 
 from .gproject import GProject
 from .repo import Repo
+
+log = logging.getLogger('|.{}'.format(__name__.split('.')[-1]))
 
 class ProjectList:
     pfile = 'projects.tsv'
@@ -44,7 +47,6 @@ class ProjectList:
             self.wiki_url = wiki
 
         if select_project:
-            #print("select " + select_project)
             self.select_project = select_project
         else:
             self.select_project = None
@@ -53,22 +55,19 @@ class ProjectList:
 
 
     def read(self, f=None):
-        ###print("f=" + f)
-
         if not f:
             return
 
         if not os.path.exists(f):
-            print("ERROR no such file " + f)
+            log.error("no such file {}".format(f))
             return
 
-        m = re.match('.*\.([a-z]+)$', f);
+        m = re.match('.+\.([a-z]+)$', f);
         if not m:
-            print("ERRR no valid extension " + f)
+            log.error("no valid extension {}".format(f))
             return
 
         ext = m.group(1)
-        ###print("ext " + ext)
         if ext == 'tsv':
             self.pfile = f
             self.read_tsv()
@@ -76,11 +75,13 @@ class ProjectList:
             self.pfile = f
             self.read_yaml()
         else:
-            print("ERROR: invalid extension " + ext)
+            log.error("invalid extension '{}' ".format(ext) )
         return
 
     def read_yaml(self,f=None, project_name=None):
-        """ read the master list in YAML format """
+        """ read the master list in YAML format
+        if project_name is specified
+        , only populate the list for that project """
 
         if self.select_project:
             project_name = self.select_project
@@ -90,11 +91,12 @@ class ProjectList:
         with open(self.pfile) as fh:
             text = fh.read()
 
-        #print("got: " + text)
         cfg = yaml.load(text)
-        ###print(cfg)
+        #log.debug("config={}".format(cfg) )
         if 'repo_root' in cfg:
             self.repo_root = cfg['repo_root']
+        else:
+            log.error("No repo root defined")
 
         if 'wiki_root' in cfg:
             self.wiki_url = cfg['wiki_root']
@@ -106,28 +108,28 @@ class ProjectList:
             self.build_root = cfg['build_root']
 
         for p in cfg['projects']:
-            if not p['name']:
-                print("ERROR no name in project " + p)
+            if not 'name' in p or not p['name']:
+                log.error("no name in project {} ".format(p))
                 continue
 
             if project_name != None and project_name != p['name']:
-                ###print("skip")
                 continue
 
             if  'code' in p:
-                cfg['code']  = p['name'].lower()
+                code   = p['code']
+            else:
+                log.debug("default code")
+                code = p['name'].lower()
 
-            gp = GProject(p['name'], p['code'], wiki=self.wiki_url )
+            gp = GProject(p['name'], code, wiki=self.wiki_url )
             if 'prefix' in p and p['prefix']:
                 gp.prefix = p['prefix']
 
             if not 'repos' in p:
-                print( p['name'] + " no repos")
+                log.error( p['name'] + " no repos")
                 continue
 
             for r in p['repos']:
-                #print("repo: ", r)
-
                 if 'name' in r:
                     dir = os.path.join(self.repo_root, r['name'])
                 if 'release_path' in r:
@@ -138,7 +140,6 @@ class ProjectList:
                     tr = True
                 if 'primary' in r:
                     gp.set_primary_repo(dir)
-                    print("v=" + gp.release)
                 else:
                     gp.add_repo(dir, tracking=tr)
 
@@ -149,26 +150,22 @@ class ProjectList:
 
 
     def read_tsv(self,f=None):
-        """ read the master list of Git Projects"""
+        """ read the master list of Git Projects in TSV format"""
         if f:
             self.pfile = f
 
         n = 0
         cols = {}
-        #print(("read: " + self.pfile))
         with open(self.pfile, 'rb') as csvfile:
             freader  = csv.reader(csvfile, delimiter="\t", quotechar='|')
             for row in freader:
                 if len(row) == 0:
-                    print("skip")
                     continue
 
                 n += 1
-
-                #print("row,", n)
                 if n == 1:
                     cols = row
-                    #print("header", cols)
+                    log.debug("header".format(cols) )
                     continue
 
                 # 0)
@@ -195,8 +192,6 @@ class ProjectList:
 
                 # 3) build dir
                 if len(row) >= 4 and row[3]:
-                    ###print("set b", row[3])
-                    #self.projects[name]['build_path'] = row[3]
                     build_dir = row[3]
                 else:
                     # default to code
@@ -206,18 +201,9 @@ class ProjectList:
                 if len(row) >= 5 and row[4]:
                     gp.prefix = row[4]
 
-                #gp.show()
-
-                # 5) wiki
-                #print("w:" + row[5] )
-
-
                 if n >= 3:
                     #break
                     pass
-
-                # 6) target (NOT USED)
-
                 # 7) rel
                 if len(row) >= 8 and row[7]:
                     gp.rel_path  = row[7]
@@ -227,7 +213,7 @@ class ProjectList:
                 gp.set_release_file();
                 gp.read_release();
                 if gp.release_file:
-                    #print( "file: " + gp.release_file )
+
                     pass
 
                 if  len(row) >= 6 and row[5]:
@@ -235,6 +221,8 @@ class ProjectList:
 
                 self.projects[name] = gp
 
+                # remaining items in row
+                # results = [gp.add_repo(os.path.join( self.repo_root, row[k] ) for k in range(8, len(row) if row[k]) ]
                 for k in range(8,len(row)):
                     #print("related:", row[k])
                     if not row[k]:
@@ -246,15 +234,14 @@ class ProjectList:
         """ return the specified GProject object """
         if not name:
             if not self.select_project:
-                print("no project specified")
+                log.error("no default project name specified")
                 return
             name = self.select_project
 
         if name in self.projects:
             return self.projects[name]
 
-        print( "%d projects " % (len(self.projects)) )
-
+        log.debug( "project {} not found in {} projects ".format(name, len(self.projects)) )
         return None
 
     def num(self):
